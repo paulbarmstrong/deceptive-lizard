8c99b77c7fa9a91f85b2f1b39df81ef5fdcf64bc
Paul Armstrong <paul83637@gmail.com>
2025-12-29 00:41:46 +0000

    fix text for 1 vote

--- packages/webapp/src/components/panels/PlayersPanel.tsx
+++ packages/webapp/src/components/panels/PlayersPanel.tsx
@@ -1,5 +1,6 @@
 import { Lobby, Player, WsUpdateRequestData } from "common"
 import { BACKGROUND_SHADE_T1 } from "../../utilities/Constants"
+import { s } from "../../utilities/Misc"
 
 interface Props {
 	lobby: Lobby,
@@ -21,15 +22,18 @@ export function PlayersPanel(props: Props) {
 	}
 
 	return <div style={{display: "flex", justifyContent: "center", gap: 10}}>
-		{props.lobby.players.map((player, playerIndex) => <div key={player.connectionId} style={{display: "flex", flexDirection: "column", alignItems: "center" , padding: 10, backgroundColor: BACKGROUND_SHADE_T1, borderRadius: 4, gap: 5}}>
-			<div style={{fontWeight: "bold"}}>{player.name}</div>
-			<div>{player.topicHint !== undefined ? `"${player.topicHint}"` : "-"}</div>
-			<div style={{display: "flex", alignItems: "center", gap: 10}}>
-				<span style={{padding: 4}}>
-					{props.lobby.players.filter(voter => voter.votePlayerIndex === playerIndex).length} votes
-				</span>
-				<span className="material-symbols-outlined" onClick={() => changeVote(playerIndex)} style={{padding: 5, cursor: "pointer", fontVariationSettings: playerIndex === props.player?.votePlayerIndex ? "'FILL' 1" : undefined}}>thumb_up</span>
+		{props.lobby.players.map((player, playerIndex) => {
+			const numVotes = props.lobby.players.filter(voter => voter.votePlayerIndex === playerIndex).length
+			return <div key={player.connectionId} style={{display: "flex", flexDirection: "column", alignItems: "center" , padding: 10, backgroundColor: BACKGROUND_SHADE_T1, borderRadius: 4, gap: 5}}>
+				<div style={{fontWeight: "bold"}}>{player.name}</div>
+				<div>{player.topicHint !== undefined ? `"${player.topicHint}"` : "-"}</div>
+				<div style={{display: "flex", alignItems: "center", gap: 10}}>
+					<span style={{padding: 4}}>
+						{numVotes} vote{s(numVotes)}
+					</span>
+					<span className="material-symbols-outlined" onClick={() => changeVote(playerIndex)} style={{padding: 5, cursor: "pointer", fontVariationSettings: playerIndex === props.player?.votePlayerIndex ? "'FILL' 1" : undefined}}>thumb_up</span>
+				</div>
 			</div>
-		</div>)}
+		})}
 	</div>
 }
